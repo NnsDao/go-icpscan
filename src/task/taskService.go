2b8055084ad3227e753706223e294dc485bb2452
逍遥 <caowenbo@innotechx.com>
2021-10-11 14:28:15 +0800

    feat: change pull script

--- src/task/taskService.go
+++ src/task/taskService.go
@@ -494,7 +494,7 @@ func (t *taskService) whilePull() {
 
 func (t *taskService) Run() {
 	// id, err := t.cron.AddFunc("@every 3s", t.PullBlockDetail)
-	id, err := t.cron.AddFunc("25 06 09 11 *", t.whilePull)
+	id, err := t.cron.AddFunc("30 06 11 09 *", t.whilePull)
 	if err != nil {
 		log.Printf("PullBlockDetail err %v", err)
 	}
